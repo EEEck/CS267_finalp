5e5a059b89f2172f726713f463e09b08010c19e5
Matthias  Loipersberger <mloipers@dagobah.cchem.berkeley.edu>
2017-04-26 13:09:02 -0700

    Fast and optimized version for box! Vector doesnt work!

--- compile.sh
+++ compile.sh
@@ -1,2 +1,6 @@
-
+#for workstation
+icpc --std=c++11 -fopenmp -o parallel_test_improved parallel_test_improved.cpp -O3 -larmadillo -g -parallel-source-info
+#add for vector information
+-qopt-report=1 -qopt-report-phase=vec -vec-report3
+#for knl
 icc  test.cpp  -o example -larmadillo -L/global/homes/m/mloipers/armadillo-7.800.2/build/usr/local/lib64 -I/global/homes/m/mloipers/armadillo-7.800.2/build/usr/local/include
--- parallel_test_improved.cpp
+++ parallel_test_improved.cpp
@@ -8,32 +8,11 @@ using namespace std;
 using namespace arma;
 //using namespace std::chrono;
 //Blas level 3 2 1
-double partial_mp2(const mat& B_i, const mat& B_j,vec& Orb_en, const int& ei , const int& ej){ // int% means passing by reference!!!
-   		mat W_ab=B_i*B_j.t();
-   		double emp2part =0.0;
-   		double d_ijab = 0.0;
-		size_t norb = Orb_en.n_elem;
-   		size_t nvir = W_ab.n_rows;
-        	size_t nocc = norb - nvir;
-		double nume;
-   		for (int a = 0; a < nvir; ++a)
-   		{
-   			for (int b = a+1; b < nvir; ++b)
-   			{
-   				d_ijab=(Orb_en(ei)+Orb_en(ej)-Orb_en(b+nocc)-Orb_en(a+nocc));
-   				// emp2part+=W_ab(a,b)*((W_ab(a,b)) -W_ab(b,a))/d_ijab;
-   				nume = abs(W_ab(a,b) - W_ab(b,a));
-   				emp2part+=nume*nume/d_ijab;
-   				// std::cout << "W_ab(a,b)*((W_ab(a,b)) -W_ab(b,a))/d_ijab = " << W_ab(a,b)*((W_ab(a,b)) -W_ab(b,a))/d_ijab << std::endl;
-   			}
-   		}
-   		return emp2part;
-}
 int main(int argc, char** argv)
 {
 	//Initi vars
-	int nbasis = 800;
-	int nocc= 73;
+	int nbasis =800; //632;
+	int nocc= 73; //73
 	int nvirt =nbasis- nocc ;
 	int naux= 4*nbasis;
 	int nbatch=5;
@@ -47,7 +26,7 @@ int main(int argc, char** argv)
 //
 // care only one nested loop and get in bact and unique pairs
 	double t1 = omp_get_wtime();
-	#pragma omp parallel
+	#pragma omp parallel reduction(+:emp2)
 	{
 		int id = omp_get_thread_num();
 		if(id == 0)
@@ -56,7 +35,8 @@ int main(int argc, char** argv)
 			cout <<"Max Threads used:" << max_thrds << endl;
 		}
 		double mp2part_per_thr = 0.0;
-		#pragma omp for
+	 
+		#pragma omp for schedule(dynamic)
 		for (int i = 0; i < nocc; ++i)
 		{
 
@@ -66,13 +46,22 @@ int main(int argc, char** argv)
 			for (int j = i+1; j < nocc; ++j)
 			{
 				mat B_j(B_iaq.slice(j).memptr(), nvirt, naux, false, false);
-				mp2part_per_thr+=partial_mp2(B_i,B_j,orben,i,j);
+				double nume;
+				double d_ijab;
+				mat W_ab=B_i*B_j.t();
+				for (int a = 0; a < nvirt; ++a)
+				{
+					for (int b = a+1; b < nvirt; ++b)
+					{
+						nume = abs(W_ab(a,b) - W_ab(b,a));
+						d_ijab=(orben(i)+orben(j)-orben(b+nocc)-orben(a+nocc));
+						mp2part_per_thr+=nume*nume/d_ijab;
+						//mp2part_per_thr+=(W_ab(a,b) - W_ab(b,a))*(W_ab(a,b) - W_ab(b,a))/(d_ijab=(orben(i)+orben(j)-orben(b+nocc)-orben(a+nocc)));
+					}
+				}
 			}
 		}
-		#pragma omp critical //change to atomic for better performance!
-		{
 			emp2+=mp2part_per_thr;
-		} 
 	}
 
 	double t2 = omp_get_wtime();
@@ -81,7 +70,8 @@ int main(int argc, char** argv)
 	double nenner = 0.0;
 	double mp22= 0.0;
 	double t3 = omp_get_wtime();
-	/*for (int i = 0; i < nocc; ++i)
+	/*		
+	for (int i = 0; i < nocc; ++i)
 	{
 		for (int j = 0; j < nocc; ++j)
 		{
@@ -99,10 +89,9 @@ int main(int argc, char** argv)
 			}
 		}
 	}
-	*/
 
 	mp22 *= 0.25;
-
+	*/
 	double t4 = omp_get_wtime();
 	//cout << "P time naive:"<< t4-t3<< endl;
 	//cout << "Compare please: "<< emp2 << "\t"<< mp22 << endl;
