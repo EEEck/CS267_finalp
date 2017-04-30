925d071680456ab7430fcc5c686185a3cf1abb33
Matthias  Loipersberger <mloipers@dagobah.cchem.berkeley.edu>
2017-04-29 18:16:32 -0700

    annoying

--- scaling.sh
+++ scaling.sh
@@ -1,25 +1,22 @@
-#!bin/bash
+$ cat scaling.sh
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p regular
+#SBATCH -C knl,quad,cache
+#SBATCH -J first_test
+#SBATCH -t 01:59:20
 
-export OMP_NUM_THREADS=1
-./parallel_test > slow.out
-./parallel_test_improved >fast.out
+#this is the KNL script
+echo ncores,nht,arch,time > threadscale_knl.csv
+for nc in 1 2 4 8 16 32 64; do
+for nht in 1 2 4; do
 
-export OMP_NUM_THREADS=2
-./parallel_test >> slow.out
-./parallel_test_improved >>fast.out
+export OMP_NUM_THREADS=$(( ${nc} * ${nht} ))
+export OMP_PLACES=cores"(${nc})"
+export OMP_PROC_BIND=spread
+#module load memkind 
+echo $nc $nht >> output
+srun -n 1 -c 272 --cpu_bind=cores ./fast >> output
+done
+done
 
-export OMP_NUM_THREADS=4
-./parallel_test >> slow.out
-./parallel_test_improved >>fast.out
-
-export OMP_NUM_THREADS=8
-./parallel_test >> slow.out
-./parallel_test_improved >>fast.out
-
-export OMP_NUM_THREADS=16
-./parallel_test >> slow.out
-./parallel_test_improved >>fast.out
-
-export OMP_NUM_THREADS=32
-./parallel_test >> slow.out
-./parallel_test_improved >>fast.out
